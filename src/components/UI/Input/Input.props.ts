7fcfc2666a6d2418b5e2fc0a6960c138751c9282
vitalii <vilavren@gmail.com>
2023-07-07 21:04:03 +0500

    added: component Input

---
+++ src/components/UI/Input/Input.props.ts
@@ -0,0 +1,6 @@
+import { DetailedHTMLProps, InputHTMLAttributes, ReactNode } from 'react'
+
+export type InputProps = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>
---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,12 @@
+import cn from 'classnames'
+import React from 'react'
+
+import styles from './Input.module.css'
+import { InputProps } from './Input.props'
+
+export const Input: React.FC<InputProps> = ({
+  className,
+  ...props
+}: InputProps) => {
+  return <input className={cn(className, styles.input)} {...props} />
+}
